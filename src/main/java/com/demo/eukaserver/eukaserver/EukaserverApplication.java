c55f96a3b7b96a993107597d4dd10ab362273a8a
Walter <lalala19@126.com>
2020-04-02 21:54:23 +0800

    new

---
+++ src/main/java/com/demo/eukaserver/eukaserver/EukaserverApplication.java
@@ -0,0 +1,14 @@
+package com.demo.eukaserver.eukaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class EukaserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EukaserverApplication.class, args);
+	}
+}
