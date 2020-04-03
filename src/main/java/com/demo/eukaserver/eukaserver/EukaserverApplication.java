47020b4f73f984bdcad3f75525ab4d682d640155
Walter <lalala19@126.com>
2020-04-03 13:23:55 +0800

    m

--- src/main/java/com/demo/eukaserver/eukaserver/EukaserverApplication.java
+++ src/main/java/com/demo/eukaserver/eukaserver/EukaserverApplication.java
@@ -8,6 +8,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 public class EukaserverApplication {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(EukaserverApplication.class, args);
 	}
